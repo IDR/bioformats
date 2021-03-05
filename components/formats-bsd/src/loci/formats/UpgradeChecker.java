55bcf78bf629ab5dfc5f3e4f677043f11cbc4a57
Sebastien Besson <seb.besson@gmail.com>
2021-03-05 11:32:07 +0000

    Bump version to 0.6.6

--- components/formats-bsd/src/loci/formats/UpgradeChecker.java
+++ components/formats-bsd/src/loci/formats/UpgradeChecker.java
@@ -67,7 +67,7 @@ public class UpgradeChecker {
   /**
    * @deprecated  As of release 6.6.0
    */
-  public static final String STABLE_VERSION = "0.6.5";
+  public static final String STABLE_VERSION = "0.6.6";
 
   /** Location of the OME continuous integration server. */
   public static final String CI_SERVER = "http://ci.openmicroscopy.org";
